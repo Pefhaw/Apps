5c218e1c119059589142fdcd661caa14a8c52983
Ajith <Ajith>
2014-04-20 09:04:19 +0530

    Modified on 20 April 2014 by Ajith

--- PlanningTable/src/com/pefhaw/planningtable/EditActivity.java
+++ PlanningTable/src/com/pefhaw/planningtable/EditActivity.java
@@ -1,16 +1,21 @@
 package com.pefhaw.planningtable;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class EditActivity extends Activity {
+	Button buttonSave1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit);
+		//buttonSave1 = (Button)findViewById(R.id.buttonSave);
 	}
 
 	@Override
@@ -25,7 +30,19 @@ public class EditActivity extends Activity {
 	    }   
 	    return true;
 	}
-	
-	
-	
+	public void buttonSaveClick(View view)  {
+		String PersonName = "textPersonName";
+		// Create or retrieve the shared preference object.
+		int mode = Activity.MODE_PRIVATE;
+		SharedPreferences SharedPreferences = getSharedPreferences(PersonName,mode);
+		// Retrieve an editor to modify the shared preferences.
+		SharedPreferences.Editor editor = SharedPreferences.edit();
+		// Store new primitive types in the shared preferences object.
+		editor.putString("textPersonID", "123456789");
+		editor.putString("textPhone", "123456789");
+		editor.putString("textEmailAddress", "123456789@123456789");
+		editor.putString("textDetails", "ABCDEFGHI");
+		// Commit the changes.
+		editor.commit();
+	}
 }
